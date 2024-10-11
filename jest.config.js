80c5f6faf61cc5e67388923948f3c0e53235d216
Jose Tomas Hernandez Lopez <joseto.lopez00@gmail.com>
2024-10-11 17:11:14 +0200

    setup dependencies

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const config = {
+  verbose: true,
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  passWithNoTests: true
+};
+
+export default config;
