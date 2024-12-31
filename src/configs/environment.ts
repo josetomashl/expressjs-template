10d69e3a12f30c4e997df045fd6742d11a138407
Jose Tomas Hernandez Lopez <joseto.lopez00@gmail.com>
2024-12-31 17:36:34 +0100

    fix module config

--- src/configs/environment.ts
+++ src/configs/environment.ts
@@ -5,5 +5,5 @@ const { NODE_ENV = 'development', PORT = 3000, JWT_SECRET = '' } = process.env;
 export const environment = {
   MODE: NODE_ENV,
   PORT: Number(PORT),
-  JWT_SECRET: String(JWT_SECRET)
+  JWT_SECRET: JWT_SECRET
 };
