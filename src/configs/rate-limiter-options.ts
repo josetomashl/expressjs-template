f4e7d79300f6281e722188daf7e2f87840a30aa2
José Tomás Fiscoo <josetomas.hl@fiscoo.es>
2025-07-22 11:35:33 +0200

    add requests rate limiter to mitigate DDoS attacks & more

---
+++ src/configs/rate-limiter-options.ts
@@ -0,0 +1,9 @@
+import type { Options } from 'express-rate-limit';
+
+export const rateLimiterOptions: Partial<Options> = {
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+};
